97a51e496bcd86708e16f818fa7c19788e28fa44
Truong Viet Trung <viettrung9012@yahoo.com>
2015-10-11 01:51:08 +0800

    Fix sketch.js eraser

--- client/sketch.js
+++ client/sketch.js
@@ -170,8 +170,8 @@ var __slice = Array.prototype.slice;
     draw: function(action) {
       var oldcomposite;
       oldcomposite = this.context.globalCompositeOperation;
-      this.context.globalCompositeOperation = "copy";
-      action.color = "rgba(0,0,0,0)";
+      this.context.globalCompositeOperation = "destination-out";
+      action.color = "#000000";
       $.sketch.tools.marker.draw.call(this, action);
       return this.context.globalCompositeOperation = oldcomposite;
     }
